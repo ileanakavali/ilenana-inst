69bbfcf5b33cc3268aa5a38d817f9744957616c1
ileanakavali <113621297+ileanakavali@users.noreply.github.com>
2023-03-01 20:37:40 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -189,19 +189,7 @@ async def upload(m, bot, chat_id, dir):
     await bot.send_message(
         chat_id=chat_id,
         text=f"Succesfully Uploaded {up} Files to Telegram.\nIf you found me helpfull Join My Updates Channel",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-					InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122")
-				],
-				[
-					InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
-                    InlineKeyboardButton("⚡️Update Channel", url="https://t.me/subin_works")
-				]
-			]
-			)
-		)
+        
     total=TOTAL
     up=0
     rm=TOTAL
